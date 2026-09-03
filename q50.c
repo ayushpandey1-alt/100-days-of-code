49e7cf7ee57bbdce5aa1c7fe4f79ed6f9385fbf3
Ayush Pandey <ayushpandey1389502@gmail.com>
2026-09-03 22:39:24 +0530

    Add files via upload

---
+++ q50.c
@@ -0,0 +1,27 @@
+/*Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = n; i >= 1; i--) {
+
+        for (j = 1; j <= i; j++) {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
