37ffd8e76d14b48eecd9d47d175e961192694859
Ayush Pandey <ayushpandey1389502@gmail.com>
2026-09-06 22:53:10 +0530

    Implement array input and output in q56.c

---
+++ q56.c
@@ -0,0 +1,22 @@
+//Read and print elements of a one-dimensional array.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read elements
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Print elements
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
