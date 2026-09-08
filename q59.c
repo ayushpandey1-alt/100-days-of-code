0ec64563b30d60387a33f7fb9f2bbf6c0e19da1d
Ayush Pandey <ayushpandey1389502@gmail.com>
2026-09-08 22:01:51 +0530

    count even and odd numbers

---
+++ q59.c
@@ -0,0 +1,31 @@
+//Count even and odd numbers in an array.
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    int even = 0, odd = 0;
+    
+    printf("Enter the no of array : ");
+    scanf("%d", &n);
+
+    // Read array elements
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Count even and odd numbers
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0) {
+            even++;
+        }
+        else {
+            odd++;
+        }
+    }
+
+    printf("Even=%d, Odd=%d", even, odd);
+
+    return 0;
+}   
