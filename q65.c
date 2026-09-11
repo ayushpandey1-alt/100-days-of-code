b279e4ad65142936f023b5b1fe751e9cde0838fa
Ayush Pandey <ayushpandey1389502@gmail.com>
2026-09-11 23:06:06 +0530

    binary search in array

---
+++ q65.c
@@ -0,0 +1,52 @@
+//Q65: Search in a sorted array using binary search.
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, key;
+    int arr[100];
+    int low, high, mid;
+    int found = 0;
+
+    scanf("%d", &n);
+
+    // Input array
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+        {
+            printf("Found at index %d", mid);
+            found = 1;
+            break;
+        }
+        else if(arr[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("-1");
+    }
+
+    return 0;
+}
