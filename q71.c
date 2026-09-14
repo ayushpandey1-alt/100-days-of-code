ff1c398a271bcefb22ffc05fa5195500fefd4a6f
Ayush Pandey <ayushpandey1389502@gmail.com>
2026-09-14 22:47:53 +0530

    Read and print a matrix.

---
+++ q71.c
@@ -0,0 +1,27 @@
+//Read and print a matrix.
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+    printf("Enter the matrix : ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+
+    // Read matrix
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print matrix
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
